0be83d432a6d577f4c93128a0a47f88f1dd3c19c
Mikhail Brinchuk <thecentury@gmail.com>
2023-05-03 21:24:45 +0200

    Removed the copied Avalonia.FuncUI

--- Avalonia.FuncUI/Assembly.fs
+++
@@ -1,8 +0,0 @@
-namespace Avalonia.FuncUI.Assembly
-
-open System.Runtime.CompilerServices
-
-[<assembly: InternalsVisibleTo("Avalonia.FuncUI.UnitTests")>]
-[<assembly: InternalsVisibleTo("Avalonia.FuncUI.Benchmarks")>]
-[<assembly: InternalsVisibleTo("Avalonia.FuncUI.Diagnostics")>]
-do()
